ad1a63eab062f7dcbd354eb1d29e578ebc82c811
benrey <ben.rey@gmail.com>
2024-09-07 10:35:54 -0500

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+<<<<<<< Updated upstream
 import Image from "next/image";
+=======
+import Link from "next/link"
+import { Button } from "@/components/ui/button"
+import { Label } from '@radix-ui/react-label';
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import { Card, CardContent } from "@/components/ui/card"
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
+import { Input } from "@/components/ui/input"
+import { Textarea } from "@/components/ui/textarea"
+import { JSX, SVGProps } from "react";
+
+>>>>>>> Stashed changes
 
 export default function Home() {
   return (
